34fe43b9e29849b9064ba167bd2dc10c4a5f97c0
Leandro Campos <camposweb@gmail.com>
2023-11-12 18:18:26 -0300

    Edit msw

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 
-async function enableMocking() {
-  if (import.meta.env.VITE_APP_DEV !== 'development') {
-    return
-  }
-
-  const { worker } = await import('./mocks/browser.ts')
-
-  return worker.start()
-}
-
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-})
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
